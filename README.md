5c6f8e6652a97e1e4f57ae7a3d2c896e19ea2ee9
Jean-TKG <theknightgamer21@gmail.com>
2022-06-13 15:18:37 +0300

    update README

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # SuperHub
+
+## Features 
+Hub Essentials:
+- Custom action based announcements
+- Launchpads
+- Double jump
+- Player Hider
+
+Extra Commands:
+- /fly
+- /setlobby
+- /lobby
+- /vanish
+
+Custom Join Events:
+- Join title
+- Custom Messages
+- Join sound
+- Join messages
+- Custom spawn location
+
+World Management:
+- Disable block breaking
+- Disable block placement
+- Disable block interaction
+- Disable fall damage
+- Disable void death (teleport to spawn)
+- Disable item pickup
+- Disable item drop
+- Disable player PvP
+- Disable hunger loss
+
+Chat Management:
+- Command blocker (/pl, /?, etc)
+- Chat lock & unlock
+- Clear chat
+- /chat clear
+- /chat lock
+- /chat unlock
--- src/ninjaknights/SuperHub/listeners/CommandListener.php
+++ src/ninjaknights/SuperHub/listeners/CommandListener.php
@@ -8,18 +8,12 @@ use pocketmine\event\Listener;
 class CommandListener implements Listener
 {
 
-    private mixed $commands;
-
-    public function __construct()
-    {
-        $this->commands = Main::getInstance()->getConfig()->getNested("command_block.blocked_commands", []);
-    }
-
     public function removeDefaultCommands(): void
     {
+        $commands = Main::getInstance()->getConfig()->getNested("command_block.blocked_commands", []);
         if(Main::getInstance()->getConfig()->getNested("command_block.enabled", true)) {
             $map = Main::getInstance()->getServer()->getCommandMap();
-            foreach ($this->commands as $cmdname) {
+            foreach ($commands as $cmdname) {
                 $cmd = $map->getCommand($cmdname);
                 if ($cmd != null) {
                     $map->unregister($cmd);
